06d320a96043ce267d31aaa008450c600e3b032b
Gaddam Rahul Kashyap <grkashyap@gmail.com>
2017-11-24 14:43:58 +0530

    Added styling

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ var express = require('express');
 var app = express();
 var PORT_TO_LISTEN = 3000;
 
-app.set("view engine","ejs")
+app.set("view engine","ejs");
+
+var nearbyPlaces = [
+    {
+        name: "Charminar",
+        image: "http://www.culturalindia.net/iliimages/Charminar-ili-45-img-4.jpg"
+    },
+    {
+        name: "Birla Mandir",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/0f/f3/ea/93/birla-mandir-in-hyderabad.jpg"
+    }
+];
 
 app.get("/", function(req,res) {
-    res.render("home");
+    res.render("home",{places:nearbyPlaces});
 });
 
 app.listen(PORT_TO_LISTEN, function() {
