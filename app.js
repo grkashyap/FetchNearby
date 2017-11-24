158013c258179574959f16b703a5df50b4684ac0
Gaddam Rahul Kashyap <grkashyap@gmail.com>
2017-11-24 10:09:27 +0530

    Added header and footer partials

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var PORT_TO_LISTEN = 3000;
 
 app.set("view engine","ejs")
 
@@ -7,6 +8,6 @@ app.get("/", function(req,res) {
     res.render("home");
 });
 
-app.listen(3000, function() {
-    console.log("Fetch Nearby App has started successfully");
+app.listen(PORT_TO_LISTEN, function() {
+    console.log("Fetch Nearby App has started listening on port "+PORT_TO_LISTEN);
 });
